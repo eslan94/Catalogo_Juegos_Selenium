5eb2435436a6ec13b87c7fa40675c59ded9a387e
eslan94 <eslan94.eca@gmail.com>
2026-03-25 19:37:59 -0500

    nuevo proyecto

--- backend/src/main/java/com/catalogojuegos/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.catalogojuegos.repository;
-
-import com.catalogojuegos.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
